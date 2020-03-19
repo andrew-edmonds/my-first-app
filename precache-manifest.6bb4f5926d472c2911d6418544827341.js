132a5269d0a68bba107f351cc850f2a54b9e3926
Andrew Edmonds <andrew.edmonds93@gmail.com>
2020-03-18 19:05:21 -0500

    Updates

---
+++ precache-manifest.6bb4f5926d472c2911d6418544827341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d3f9976c09ada1b824fc1b3446075b",
+    "url": "/my-first-app/index.html"
+  },
+  {
+    "revision": "b2380bf1835a2a60fd4a",
+    "url": "/my-first-app/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "08091a2e86c4a8d7ead8",
+    "url": "/my-first-app/static/css/main.55d607cf.chunk.css"
+  },
+  {
+    "revision": "b2380bf1835a2a60fd4a",
+    "url": "/my-first-app/static/js/2.2875678e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-first-app/static/js/2.2875678e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08091a2e86c4a8d7ead8",
+    "url": "/my-first-app/static/js/main.892c4341.chunk.js"
+  },
+  {
+    "revision": "60ef5d601a1827203db5",
+    "url": "/my-first-app/static/js/runtime-main.64ed722e.js"
+  }
+]);
